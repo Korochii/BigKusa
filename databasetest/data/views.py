c4e8abd01afe92ea40106b2467980308afc98b55
Korochii <e0543630@DESKTOP-FM88NND.localdomain>
2021-05-18 22:10:55 +0800

    Deleted files

--- databasetest/data/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import Entry
-
-# Create your views here.
-def index(request):
-    return render(request, "data/index.html", {
-        "data": Entry.objects.all()
-    })
