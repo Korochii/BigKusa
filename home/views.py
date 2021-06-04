4d105e8d34599bd6659489affb15d114d175e5e6
Korochi <yanlong21@live.com.sg>
2021-06-04 20:17:46 +0800

    Fixed login bypass bug

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from googletrans import Translator
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, "home/index.html")
 
-
+@login_required
 def translated(request):
     inputText = request.GET.get('text')
     inputLang = request.GET.get('inLanguage')
--- log/views.py
+++ log/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     """View function for home page of site."""
     context = {
