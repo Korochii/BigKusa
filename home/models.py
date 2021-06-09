94172db8cfe2a63ea717c1f24afafcf9c3d2e7d9
Korochi <yanlong21@live.com.sg>
2021-06-09 22:44:13 +0800

    Testing save feature for database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Entry(models.Model):
+    owner = models.TextField('Owner')
+    date_added = models.DateTimeField('Date Added', auto_now_add=True)
+    input_text = models.TextField('Text')
+    output_text = models.TextField('Translation')
+    language = models.CharField('Language', max_length=120)
+    remarks = models.TextField('Remarks', blank=True)
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from googletrans import Translator
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from home.models import Entry
 # Create your views here.
 
 @login_required
@@ -10,11 +11,18 @@ def index(request):
 
 @login_required
 def translated(request):
+    user = request.user.id # Retrieve user id
     inputText = request.GET.get('text')
     inputLang = request.GET.get('inLanguage')
     outputLang = request.GET.get('outLanguage')
     tler = Translator()
     outputResult = tler.translate(inputText, outputLang, inputLang) # return type is googletrans.models.Translated
     outputText = outputResult.text
-    return render(request, "home/translated.html", {'input': inputText, 'output': outputText})
+    context = {
+        'owner' : user,
+        'input_text' : inputText,
+        'output_text' : outputText,
+        'language' : inputLang + '->' + outputLang,
+    }
+    return render(request, "home/translated.html", context)
 
