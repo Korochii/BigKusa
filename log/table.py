07dbc6f2fc1bbb958828ff345aae133e8bb46b17
Korochi <yanlong21@live.com.sg>
2021-06-15 02:45:49 +0800

    Replaced django-tables2 with DataTables

--- log/table.py
+++
@@ -1,9 +0,0 @@
-import django_tables2 as tables
-from home.models import Entry
-
-class EntryTable(tables.Table):
-    class Meta:
-        model = Entry
-        exclude = ("id", "user")
-        template_name = "django_tables2/bootstrap4.html"
-    
--- log/views.py
+++ log/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from home.models import Entry
-from .table import EntryTable
-from django_tables2 import RequestConfig
+
 
 # Create your views here.
 @login_required
 def index(request):
-    filteredData = EntryTable(Entry.objects.filter(user=request.user).order_by('-date_added'))
-    RequestConfig(request).configure(filteredData)
+    filteredData = Entry.objects.filter(user=request.user).order_by('-date_added')
     # Render the HTML template index.html with the filtered data
     return render(request, 'log/index.html', {'data':filteredData})
