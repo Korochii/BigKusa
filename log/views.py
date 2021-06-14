a8bd98110b95dc1626b7301a3b5fec9b291612b2
Korochi <yanlong21@live.com.sg>
2021-06-14 22:51:51 +0800

    Added basic sorting feature in log

--- log/views.py
+++ log/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from home.models import Entry
 from .table import EntryTable
+from django_tables2 import RequestConfig
 
 # Create your views here.
 @login_required
 def index(request):
     filteredData = EntryTable(Entry.objects.filter(user=request.user).order_by('-date_added')) # Can use -date_added for reverse order
-    
+    RequestConfig(request).configure(filteredData)
     # Render the HTML template index.html with the filtered data
     return render(request, 'log/index.html', {'data':filteredData})
