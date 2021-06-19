0e738cd0a5b9098f6be13bbb78f1ae16197759f7
Korochi <yanlong21@live.com.sg>
2021-06-19 17:53:05 +0800

    Added delete function

--- log/urls.py
+++ log/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^task/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
--- log/views.py
+++ log/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from home.models import Entry
-
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @login_required
 def index(request):
     filteredData = Entry.objects.filter(user=request.user).order_by('-date_added')
+    
     # Render the HTML template index.html with the filtered data
     return render(request, 'log/index.html', {'data':filteredData})
+
+@login_required()
+def post_remove(request, pk):
+    post = get_object_or_404(Entry, pk=pk)
+    post.delete()
+    return HttpResponseRedirect('/log')
